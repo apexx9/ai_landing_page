f492af2ff0e5802c51640089af2706761bc2d638
apexx9 <aaronnartey001@gmail.com>
2025-03-06 23:26:53 +0000

    setting up the backend file

--- client_side/src/app/pages/api/auth/sign-in.tsx
+++ client_side/src/app/pages/api/auth/sign-in.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from '../../../styles/pages/register/_signin.module.scss'
 
+
+{/* Keep the major sign in logics here */}
 const SignIn = ({}) => {
   return (
     <div className={styles['signin']}>
--- client_side/src/app/pages/api/auth/sign-up.tsx
+++ client_side/src/app/pages/api/auth/sign-up.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import styles from '../../../styles/pages/register/_signup.module.scss';
 
+
+{/* Keep the sign up logics cv in here and then interface with the backend using the api route */}
+
 const SignUp = ({}) => {
   return (
     <div className={styles['signup']}>
